4ffeaff0ff60663e88d846445dcee66a0f426f3f
ruko <rukokarasu@gmail.com>
2025-07-28 16:00:28 -0700

    revert to cddl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use cddl_cat::validate_cbor_bytes;
+use cddl::validate_cbor_from_slice;
+use cddl::validator::Validator;
 use ciborium;
 use std::fs;
 use std::fs::File;
 
 fn main() {
-    let cddl_input = fs::read_to_string("nes.cddl").unwrap();
-    let cbor_bytes = fs::read("nes1.cbor").unwrap();
-    match validate_cbor_bytes("new_epoch_state", &cddl_input, &cbor_bytes) {
+    let args = std::env::args().collect::<Vec<String>>();
+    let cddl_filepath = &args[1];
+    let cbor_filepath = &args[2];
+    let cddl_input = fs::read_to_string(cddl_filepath).unwrap();
+    let cbor_bytes = fs::read(cbor_filepath).unwrap();
+    match validate_cbor_from_slice(&cddl_input, &cbor_bytes, None) {
         Ok(()) => println!("valid!"),
         Err(e) => println!("invalid: {}", e)
     }
